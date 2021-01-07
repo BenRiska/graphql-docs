9c65fd849faf4d5fa2b862b6ced75aa5254b735d
bentaylor <ben10taylor10@hotmail.com>
2021-01-07 18:56:45 +0000

    done

--- server.js
+++ server.js
@@ -1,19 +1,107 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+
+let members = [
+    {
+        id: "1",
+        name: "Ben",
+        currentGames: [{
+            name: "Epic seven",
+            consoles: ["PC", "Mobile"]
+        }],
+        roles: ["Unpaid Mod", "RTA Warrior"]
+    },
+    {
+        id: "2",
+        name: "Nex",
+        currentGames: [{
+            name: "Epic seven",
+            consoles: ["PC", "Mobile"]
+        }],
+        roles: ["Unpaid Mod", "RTA Warrior", "Maple Millionaire"]
+    },
+    {
+        id: "3",
+        name: "Tivrey",
+        currentGames: [{
+            name: "Epic seven",
+            consoles: ["PC", "Mobile"]
+        }],
+        roles: ["RTA Warrior"]
+    },
+    {
+        id: "4",
+        name: "Derek",
+        currentGames: [{
+            name: "Epic seven",
+            consoles: ["PC", "Mobile"]
+        }],
+        roles: ["Unpaid Mod", "Certified Clown"]
+    },
+    {
+        id: "5",
+        name: "Gobby",
+        currentGames: [{
+            name: "Epic seven",
+            consoles: ["PC", "Mobile"]
+        }],
+        roles: ["Clown of the Year"]
+    }
+]
  
 const typeDefs = gql`
+  type Game {
+      name: String!
+      consoles: [String!]!
+  }
+  type Member {
+    id: ID!
+    name: String!
+    currentGames: [Game!]
+    roles: [String!]
+  }
+  input RegistrationInput {
+    name: String!
+  }
   type Query {
-    hello: String
+    getMembers: [Member!]!
+    getMember(memberId: ID!): Member!
+  }
+  type Mutation {
+      addMember(registrationInput: RegistrationInput): Member! 
+      deleteMember(memberId: ID!): [Member!]!
   }
 `;
  
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    getMembers(){
+        return members
+    },
+    getMember(_, {memberId}){
+        const member = members.filter(member =>  member.id === memberId)[0]
+        return member
+    }
   },
+  Mutation: {
+    addMember(_, {registrationInput}, {greeting}){
+        console.log(greeting)
+        members.push({...registrationInput, id: (members.length + 1)})
+        return members[members.length - 1]
+    },
+    deleteMember(_, {memberId}){
+        members = members.filter(member => member.id !== memberId)
+        return members
+    }
+  }
 };
  
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: {greeting: "hi"} }
+    );
+
  
 const app = express();
 server.applyMiddleware({ app });
